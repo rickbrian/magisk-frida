0f2fbc3d54aadfd8b12f27bc5fcc072fada88c78
rickbrian <91403666+rickbrian@users.noreply.github.com>
2024-01-11 12:20:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,30 +18,44 @@
 import build
 import util
 
+#def main():
+#    last_frida_tag = util.get_last_frida_tag()
+#    last_project_tag = util.get_last_project_tag()
+#    last_commit_tag = util.get_last_commit_tag()
+#    new_project_tag = "0"
+#
+#    if last_frida_tag != util.strip_revision(last_project_tag) \
+#        or (last_frida_tag != util.strip_revision(last_commit_tag)
+#            and util.get_commit_message().lower() == "release"):
+#
+#        new_project_tag = util.get_next_revision(last_frida_tag)
+#        print(f"Update needed to {new_project_tag}")
+#
+#        # for use by deployment
+#        with open("NEW_TAG.txt", "w") as the_file:
+#            the_file.write(new_project_tag)
+#    else:
+#        print("All good!")
+#    print(last_frida_tag)
+#    print(last_project_tag)
+#    print(last_commit_tag)
+#    print(new_project_tag)
+#    #last_frida_tag="16.1.3"
+#    build.do_build(last_frida_tag, new_project_tag)
+
 
 def main():
-    last_frida_tag = util.get_last_frida_tag()
-    last_project_tag = util.get_last_project_tag()
-    last_commit_tag = util.get_last_commit_tag()
+
+    last_frida_tag = "16.1.3"
     new_project_tag = "0"
+    
+    new_project_tag = util.get_next_revision(last_frida_tag)
+    print(f"Update needed to {new_project_tag}")
 
-    if last_frida_tag != util.strip_revision(last_project_tag) \
-        or (last_frida_tag != util.strip_revision(last_commit_tag)
-            and util.get_commit_message().lower() == "release"):
-
-        new_project_tag = util.get_next_revision(last_frida_tag)
-        print(f"Update needed to {new_project_tag}")
-
-        # for use by deployment
-        with open("NEW_TAG.txt", "w") as the_file:
-            the_file.write(new_project_tag)
-    else:
-        print("All good!")
-    print(last_frida_tag)
-    print(last_project_tag)
-    print(last_commit_tag)
-    print(new_project_tag)
-    #last_frida_tag="16.1.3"
+    # for use by deployment
+    with open("NEW_TAG.txt", "w") as the_file:
+        the_file.write(new_project_tag)
+            
     build.do_build(last_frida_tag, new_project_tag)
 
 
