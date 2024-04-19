5f4cc7eca526aeaf7944ea5ecf6370a0de240aa5
rickbrian <91403666+rickbrian@users.noreply.github.com>
2024-04-19 10:56:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ import util
 
 def main():
 
-    last_frida_tag = "16.1.10"
+    last_frida_tag = "16.1.8"
     new_project_tag = "0"
     
     new_project_tag = util.get_next_revision(last_frida_tag)
